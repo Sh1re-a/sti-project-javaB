23530e597bd6426eefdd927830c38a1440d8b4be
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-01-31 11:14:44 +0100

    Initial commit

---
+++ src/main/java/se/sti/stiprojectjavaB/StiProjectJavaBApplication.java
@@ -0,0 +1,13 @@
+package se.sti.stiprojectjavaB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StiProjectJavaBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StiProjectJavaBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/sti/stiprojectjavaB/StiProjectJavaBApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sti.stiprojectjavaB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StiProjectJavaBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
